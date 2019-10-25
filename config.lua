6a045f6bbbbf0e141c68d2bf20c2b95a4cf87640
TaerAttO <50113216+MonsterTaerAttO@users.noreply.github.com>
2019-10-25 21:45:50 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.Locale                 = 'en'
 
 Config.VaultBox = 'p_v_43_safe_s'
 Config.Vault = {
-	MonsterVault = {coords = vector3(976.71, -2128.46, 30.48), heading = 256.0},
+	MonsterVault = {coords = vector3(240.06, -791.71, 29.50), heading = 252.27},
 	TaerAttOVault = {coords = vector3(973.71, -2128.46, 30.48), heading = 256.0},
 }
 Config.ItemLicence = 'bread' --'licence_vault'
