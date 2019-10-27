f5f2a398bcf0aea042b2561c0ab27bdd866580c5
TaerAttO <50113216+MonsterTaerAttO@users.noreply.github.com>
2019-10-27 22:56:07 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,5 +6,5 @@ Config.Vault = {
 	MonsterVault = {coords = vector3(217.89, -799.96, 29.77), heading = 247.81},
 	TaerAttOVault = {coords = vector3(219.06, -797.20, 29.75), heading = 246.09},
 }
-Config.ItemLicense = 'bread' --'licence_vault'
+Config.ItemLicense = '' --'licence_vault' -- If you don't want to use items Allow you to leave it blank or Config.ItemLicense = nil
 Config.InfiniteLicense		= false  -- Should one License last forever?
